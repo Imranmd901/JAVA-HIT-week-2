a538c84d362d8fddc77274c25fbe39822235671c
Imranmd901 <md55imranmd@gmail.com>
2021-06-17 19:52:02 +0530

    initial commit

--- Ladder/src/Ladder.java
+++ Ladder/src/Ladder.java
@@ -1,9 +1,21 @@
+import java.util.Scanner;
 
-public class Ladder {
+
+ class Ladder {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner in=new Scanner(System.in);
+        System.out.println("Enter the number of rows");
+        
+        int a=in.nextInt();
+          for(int i=1;i<=a;i++)
+          {
+        	  for(int j=1;j<=i;j++)
+        		  System.out.println(j);
+        	  for(int k=i-1;k>=1;k--)
+        		  System.out.print(k);
+        	  System.out.print("\n");
+          }
 	}
 
 }
